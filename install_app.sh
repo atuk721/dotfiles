2f53f86b919f9154c492d79c85391999bf126581
atuk <8051052+atuk721@users.noreply.github.com>
2020-04-27 22:40:31 +0200

    work

--- install_app.sh
+++ install_app.sh
@@ -2,9 +2,10 @@
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
