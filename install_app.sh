053fa4d5c05c421951189a76de247c9e70e36ff7
atuk <8051052+atuk721@users.noreply.github.com>
2020-04-28 20:08:56 +0000

    work

--- install_app.sh
+++ install_app.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
