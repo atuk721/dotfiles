a30805704c9666444e2b4036f26b675b02d0c1b2
atuk <8051052+atuk721@users.noreply.github.com>
2020-05-13 13:18:54 +0200

    work

--- install_app.sh
+++ install_app.sh
@@ -4,4 +4,11 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Go
+filename=go1.14.2.linux-amd64.tar.gz
+curl -O "https://dl.google.com/go/${filename}"
+sudo tar -C /usr/local -xzf ${filename}
+rm -f ${filename}
+
