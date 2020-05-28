96266ada8aba19a0afd766311f675448a788459b
atuk <8051052+atuk721@users.noreply.github.com>
2020-05-28 20:46:11 +0200

    work

--- install_app.sh
+++ install_app.sh
@@ -2,12 +2,13 @@
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install node
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install yarn -g
+
+nvm install node
+npm install yarn -g
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
